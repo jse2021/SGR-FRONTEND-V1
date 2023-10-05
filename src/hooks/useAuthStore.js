b240aa6d1ee2ccc150d859c06b385701557f8088
jse2021 <maldonadojose201422@gmail.com>
2023-10-05 16:48:03 -0300

    sigo

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,13 +1,24 @@
 import { useDispatch, useSelector } from "react-redux"
+import {calendarApi} from '../api'
 
 export const useAuthStore =() =>{
 
-    const{status, user, errorMessage} = useSelector(state => state.auth);
+    const{status, user, errorMessage} = useSelector(state => state.auth);    
     const dispatch = useDispatch();
 
     const startLogin = async ({email, password}) => {
         console.log({email, password});
+
+        // try {
+            
+        //     const resp = await calendarApi.post('/auth',{email,password});
+        // } catch (error) {
+        //     console.log({error})
+            
+        // }
     }
+
+   
 
     return{
         //propiedades
