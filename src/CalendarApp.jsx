21e91f22b817f9598e29e454c9f60af37f3a77c5
jse2021 <maldonadojose201422@gmail.com>
2025-04-21 16:49:54 -0300

    repasando codigo

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -3,6 +3,7 @@ import { AppRouter } from "./router/AppRouter"
 import { Provider } from 'react-redux'
 import {store} from './store'
 import React from 'react'
+import Tarjeta from "./components/Tarjeta";
 
 
 
@@ -11,12 +12,26 @@ export const CalendarApp =()=> {
     const [result, setResult] = React.useState({});
     
     return(
+<div style={{ padding: "2rem" }}>
+      <h1>Componentes con Props y Estado</h1>
 
-        <Provider store={store}>
-            <BrowserRouter>
-                <AppRouter />
-            </BrowserRouter>
-        </Provider>
+      <Tarjeta
+        titulo="React JS"
+        descripcion="Una librería para construir interfaces."
+        imagen="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+      />
+
+      <Tarjeta
+        titulo="Vite"
+        descripcion="Un bundler súper rápido para proyectos modernos."
+        imagen="https://vitejs.dev/logo.svg"
+      />
+    </div>
+        // <Provider store={store}>
+        //     <BrowserRouter>
+        //         <AppRouter />
+        //     </BrowserRouter>
+        // </Provider>
         
         
         
