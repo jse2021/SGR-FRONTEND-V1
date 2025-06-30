e75a8f47bc85c264d96e3c419d22f58118177911
jse2021 <maldonadojose201422@gmail.com>
2025-06-30 15:42:00 -0300

    produccion

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -1,21 +1,22 @@
-import axios from 'axios'
-import {getEnvVariables} from '../helpers'
+import axios from "axios";
+import { getEnvVariables } from "../helpers";
 
 /**
  * CONEXION AL BACKEND
  */
-const{VITE_API_URL} = getEnvVariables();
+const { VITE_API_URL } = getEnvVariables();
 
-    const calendarApi = axios.create({
-        baseURL:VITE_API_URL
-    })
-        //configurara receptores: nos va apermitir interceptar peticiones que van o regresan del backend - necesario para mantener la autenticacion
-        calendarApi.interceptors.request.use(config=> {
-            config.headers = {
-                ...config.headers,//esparce todos los headers que vienen en config
-                'x-token': localStorage.getItem('token')
-            }
-            return config;
-        })
+const calendarApi = axios.create({
+  //   baseURL: VITE_API_URL, //para localhost
+  baseURL: import.meta.env.VITE_API_URL, //para produccion
+});
+//configurara receptores: nos va apermitir interceptar peticiones que van o regresan del backend - necesario para mantener la autenticacion
+calendarApi.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers, //esparce todos los headers que vienen en config
+    "x-token": localStorage.getItem("token"),
+  };
+  return config;
+});
 
 export default calendarApi;
