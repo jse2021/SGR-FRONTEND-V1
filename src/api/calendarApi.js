b8cc07b5c318b61d5873d318588c6d1e98dd00bb
jse2021 <maldonadojose201422@gmail.com>
2025-06-30 15:58:33 -0300

    produccion

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -9,7 +9,7 @@ const { VITE_API_URL } = getEnvVariables();
 const calendarApi = axios.create({
   //   baseURL: VITE_API_URL, //para localhost
   //   baseURL: import.meta.env.VITE_API_URL, //para produccion
-  VITE_API_URL: import.meta.env.VITE_API_URL,
+  baseURL: VITE_API_URL, // ✅ funciona tanto en localhost como en producción
 });
 //configurara receptores: nos va apermitir interceptar peticiones que van o regresan del backend - necesario para mantener la autenticacion
 calendarApi.interceptors.request.use((config) => {
