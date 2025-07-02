7519e14f1a7f069d6e4a11277d5a7ecdbe411ae7
jse2021 <maldonadojose201422@gmail.com>
2025-07-02 11:37:20 -0300

    estoy corsx

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -5,10 +5,10 @@ import { getEnvVariables } from "../helpers";
  * CONEXION AL BACKEND
  */
 const { VITE_API_URL } = getEnvVariables();
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+// const baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
 
 const calendarApi = axios.create({
-  baseURL, // Base URL dinámica
+  baseURL: VITE_API_URL, // Base URL dinámica
 });
 // const calendarApi = axios.create({
 //   //   baseURL: VITE_API_URL, //para localhost
