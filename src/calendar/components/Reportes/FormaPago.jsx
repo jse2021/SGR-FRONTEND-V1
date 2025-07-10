642436a8aadaed51dc938c26f028153f586ccb7f
jse2021 <maldonadojose201422@gmail.com>
2025-07-10 16:22:35 -0300

    terminando diseño formas de pago

--- src/calendar/components/Reportes/FormaPago.jsx
+++ src/calendar/components/Reportes/FormaPago.jsx
@@ -1,16 +1,71 @@
-import React from 'react'
-import { Navbar } from '../Navbar';
+import React from "react";
+import { Navbar } from "../Navbar";
+import DatePicker from "react-datepicker";
+import AsyncSelect from "react-select/async";
 
 export const FormaPago = () => {
   return (
     <>
-    <Navbar />
-    <div>
-      <h1>Estado de Formas de Pago</h1>
-      <h1>En construcción...</h1>
-    </div>
+      <Navbar />
+      <h1 className="text-center my-4">Control Formas de Pago</h1>
+      <div className="col-md-8 login-form-3 mx-auto">
+        <form>
+          <div className="row align-items-end g-2">
+            {/* Fecha */}
+            <div className="col-md-3">
+              <DatePicker
+                className="form-control w-100"
+                placeholderText="Seleccionar fecha"
+                dateFormat="dd/MM/yyyy"
+                locale="es"
+              />
+            </div>
+
+            {/* Cancha */}
+            <div className="col-md-3">
+              <AsyncSelect
+                classNamePrefix="react-select"
+                className="w-100"
+                name="cancha"
+                placeholder="Cancha"
+                defaultOptions
+              />
+            </div>
+
+            {/* Forma de pago */}
+            <div className="col-md-3">
+              <AsyncSelect
+                classNamePrefix="react-select"
+                className="w-100"
+                name="f.pago"
+                placeholder="Forma de pago"
+                defaultOptions
+              />
+            </div>
+
+            {/* Estado */}
+            <div className="col-md-3">
+              <AsyncSelect
+                classNamePrefix="react-select"
+                className="w-100"
+                name="e.pago"
+                placeholder="Estado de pago"
+                defaultOptions
+              />
+            </div>
+          </div>
+
+          <div className="text-center mt-4">
+            <input
+              type="button"
+              className="btn btn-dark px-5 py-2 rounded-pill"
+              value="Buscar"
+            />
+          </div>
+        </form>
+      </div>
     </>
-    
   );
-}
+};
+
 export default FormaPago;
